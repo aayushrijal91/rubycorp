e0355619afa793b59f5e942f84e08eb4b3589ff9
Aayush Rijal <aayush.rijal99@gmail.com>
2022-06-29 16:23:12 +1000

    Tracking installed

--- app/src/header.php
+++ app/src/header.php
@@ -40,6 +40,31 @@
 		})(window, document, 'script', 'dataLayer', 'GTM-MMJ532L');
 	</script>
 	<!-- End Google Tag Manager -->
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-52116368-2"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', 'AW-10940119318');
+		gtag('config', 'UA-52116368-2');
+	</script>
+
+	<!-- Global site tag (gtag.js) - Google Ads -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-10940119318"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+		gtag('config', 'AW-10940119318');
+	</script>
 </head>
 
 <body>
--- app/thankyou.php
+++ app/thankyou.php
@@ -1,6 +1,13 @@
 <div class="thankyou-wrapper text-white text-center py-3" style="background-color: green;">
     Thank you! Your message has been sent. We will contact you shortly.
 </div>
+<!-- Event snippet for Booking Form conversion page -->
+
+<script>
+
+  gtag('event', 'conversion', {'send_to': 'AW-10940119318/phIGCN7hockDEJby0-Ao'});
+
+</script>
 <?php
 include __DIR__ . '/src/main.php';
 ?>
